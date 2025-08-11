44c96ce48fb739b50c4df3e7abd7d6ca8ef2034d
Mits <mitsrael9@gmail.com>
2025-08-11 19:09:06 -0300

    create Header component

--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,36 +1,10 @@
-import { NavLink, Outlet, useLocation } from 'react-router'
-import { ApertureIcon, PlusIcon } from '@phosphor-icons/react'
-import Button from '../components/button'
-import Text from '../components/text'
-import Icon from '../components/icon'
-import Container from '../components/container'
+import { Outlet } from 'react-router'
+import Header from '../core-components/Header'
 
 export default function LayoutMain() {
-  const location = useLocation()
-  const isHome = location.pathname === '/'
-
   return (
     <>
-      <header className="bg-slate-900">
-        <Container
-          as="div"
-          className="w-full flex flex-row justify-between gap-6"
-        >
-          <NavLink to="/" className="flex items-center gap-2 h-12">
-            <Icon svg={ApertureIcon} className="w-6 h-6 fill-blue-400" />
-            <Text variant="text-xl-bold" className="text-slate-200">
-              Lens Manager
-            </Text>
-          </NavLink>
-          {isHome && (
-            <NavLink to="/create-product">
-              <Button className="w-48" icon={PlusIcon}>
-                Add Lens
-              </Button>
-            </NavLink>
-          )}
-        </Container>
-      </header>
+      <Header />
       <main>
         <Outlet />
       </main>
